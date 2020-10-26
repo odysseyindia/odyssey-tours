d875deffeb8169b55df41e460e886b65e221d602
Alfred Tuinman <alfred@ourmaninindia.com>
2020-10-26 21:47:59 +0100

    by region completed

---
+++ functions/contact/contact.js
@@ -0,0 +1,57 @@
+require('dotenv').config()
+
+const { MAILGUN_API_KEY, MAILGUN_DOMAIN, MAILGUN_URL, FROM_EMAIL_ADDRESS, CONTACT_TO_EMAIL_ADDRESS } = process.env
+
+const mailgun = require('mailgun-js')({ apiKey: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN, host: "api.eu.mailgun.net" })
+
+exports.handler = async (event, context, callback) => {
+
+// console.log(event.body)
+
+const data = JSON.parse(event.body)	
+
+var text = `Dear ${data.name}, \n\n`
+text += `Thanks a lot for your enquiry. We would love to see you visit India.\n\n`
+text += `We will get back to you as soon as possible.\n\n`
+text += `Kind regards,\n\nOdyssey Team`
+text += `\n\nYour message: \n\n`
+text += `${data.message} \n`
+text += `Country of residence: ${data.country} \n`	
+
+
+const style = "<style> </style>"
+var body = `<html>${style}<div>`
+body += `<p>Dear ${data.name},</p>`
+body += "<p>Thanks a lot for your enquiry. We would love to see you visit India. </p>"
+body += "<p>We will get back to you as soon as possible.</p>"
+body += "<p>Kind regards,</p><p>Odyssey Team</p>"
+body += `<h4>Your message:</h4><p>${data.message}</p>`
+body += `<p>Country of residence: ${data.country}</p>`
+body += `</div></html>`
+
+	let response
+	try {
+		response = await mailgun.messages().send({
+		  	to: data.email,
+		  	bcc: CONTACT_TO_EMAIL_ADDRESS,
+		  	from: FROM_EMAIL_ADDRESS,
+		  	subject: `Enquiry for Summertime from ${data.arrdate}`,
+		  	text: text,
+		  	html: body
+		})
+	} 
+	catch (error) {
+		console.log('Error: ', error)
+		return {
+		  	statusCode: error.statusCode || 500,
+		  	headers: { "Access-Control-Allow-Origin": "*" },
+		  	body: JSON.stringify({	error: error.message })
+		}
+	}
+
+	return {
+		statusCode: 200,
+		headers: { "Access-Control-Allow-Origin": "*" },
+		body: JSON.stringify({ result: 200	})
+	}
+}
